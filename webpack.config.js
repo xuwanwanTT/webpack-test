bd165131afcabbf2467113b889efcc9dd62e7247
xuwanwanTT <1054749044@qq.com>
2017-09-13 15:14:55 +0800

    页面自动刷新，webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'dist')
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ]
+  devtool: 'source-map',
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress: {
+  //       warnings: false
+  //     }
+  //   })
+  // ]
 }
